3b4294b84a4a58095c992781e63b9ccf9859015b
Andrew Muratov <andrewamuratov@gmail.com>
2026-09-09 15:31:19 -0400

    Preserve install.ps1 compatibility options

--- install.ps1
+++ install.ps1
@@ -1,19 +1,33 @@
 [CmdletBinding()]
 param(
-    [Parameter(ValueFromRemainingArguments = $true)]
-    [object[]]$RemainingArgs
+    [string]$User,
+    [Alias('Host')][string]$HostName,
+    [string]$Alias = 'utm',
+    [string]$KeyPath,
+    [switch]$SkipKeyCopy,
+    [switch]$NoHushLogin,
+    [switch]$Help
 )
 
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = 'Stop'
 
+$argsForSetup = @{}
+if ($PSBoundParameters.ContainsKey('User')) { $argsForSetup.User = $User }
+if ($PSBoundParameters.ContainsKey('HostName')) { $argsForSetup.HostName = $HostName }
+if ($PSBoundParameters.ContainsKey('Alias')) { $argsForSetup.Alias = $Alias }
+if ($PSBoundParameters.ContainsKey('KeyPath')) { $argsForSetup.KeyPath = $KeyPath }
+if ($SkipKeyCopy) { $argsForSetup.SkipKeyCopy = $true }
+if ($NoHushLogin) { $argsForSetup.NoHushLogin = $true }
+if ($Help) { $argsForSetup.Help = $true }
+
 $localSetup = Join-Path $PSScriptRoot 'setup.ps1'
 if ($PSScriptRoot -and (Test-Path $localSetup)) {
-    & $localSetup @RemainingArgs
+    & $localSetup @argsForSetup
     exit $LASTEXITCODE
 }
 
 $url = 'https://raw.githubusercontent.com/andrewmuratov/utm-shell/main/setup.ps1'
 $tmp = Join-Path $env:TEMP 'utm-shell-setup.ps1'
 Invoke-WebRequest $url -OutFile $tmp -UseBasicParsing
-& $tmp @RemainingArgs
+& $tmp @argsForSetup
